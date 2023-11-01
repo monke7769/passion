bfaf55631dc198832cd8792415d0d47f479f0731
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2023-11-01 16:16:56 -0700

    Update hex.py

--- hex.py
+++ hex.py
@@ -9,15 +9,15 @@ class hexadecimal:
   def __init__(self, text):
     self.text = text
   def encrypt(enc): # Main encryption algorithm
-    encbytes = enc.text.encode('utf-8')
-    hexverse = binascii.hexlify(encbytes)
-    hex_string = hexverse.decode('utf-8')
-    return hex_string
+    encbytes = enc.text.encode('utf-8') # Encode the text to hex bytes
+    hexverse = binascii.hexlify(encbytes) # Make the encbytesinto actual hex
+    hex_string = hexverse.decode('utf-8') # Decode into a hex string
+    return hex_string 
 
   def decrypt(dec): # Main decryption algorithm
-    dec.text = dec.text.replace("0x", "")
-    bytes_object = bytes.fromhex(dec.text)
-    pt = bytes_object.decode("ASCII")
+    dec.text = dec.text.replace("0x", "") # Get rid of the "0x" in front if present in some hex representations
+    bytes_object = bytes.fromhex(dec.text) # Get the bytes from the hex
+    pt = bytes_object.decode("ASCII") # Decode into ASCII
     return pt
 
 mycipher = hexadecimal("Hello World") 
