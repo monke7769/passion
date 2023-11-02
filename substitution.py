77a2110abd11e2d59de7c846060f54b7f67749f7
Shuban Pal <67974101+shuban-789@users.noreply.github.com>
2023-11-01 23:02:13 -0700

    Update substitution.py

--- substitution.py
+++ substitution.py
@@ -13,9 +13,9 @@ class substitution:
     for i in range(0,len(enc.text)):
       if enc.text[i] in alphabet:
         for j in range(0,len(alphabet)): # Replace character with character at the same position but from the key alphabet
-          if enc.text[i] == alphabet[j]:
+          if enc.text[i] == alphabet[j]: # Parse through alphabet to find matching character
             index = j
-        newchar = enc.key[index]
+        newchar = enc.key[index] # Replace with the corresponding positioning for the new alphabet
         ct += newchar
       else:
         ct += enc.text[i]
@@ -24,7 +24,7 @@ class substitution:
   def decrypt(dec):
       alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
       pt = ""
-      for i in range(0, len(dec.text)):
+      for i in range(0, len(dec.text)): # Using new alphabet, use corresponding position to replace characters
         if dec.text[i] in alphabet:
           for j in range(0, len(alphabet)):
             if dec.text[i] == dec.key[j]:
@@ -36,5 +36,5 @@ class substitution:
       return pt
 
 
-mycipher = substitution("QWERTYUIOPASDFGHJKLZXCVBNM","ITSSG VGKSR") # Test case: Given a key alphabet and ciphertext, decrypt the ciphertext.
+mycipher = substitution("QWERTYUIOPASDFGHJKLZXCVBNM","ITSSG VGKSR") # Test case: Given a shifted key alphabet and ciphertext, decrypt the ciphertext.
 print(mycipher.decrypt())
